6a402956ad1ab790ea65a0cec265a85d01175a7b
Nisal Muthugala <nmmuthugala@gmail.com>
2018-08-06 10:17:21 +0530

    Intial commit

---
+++ src/main/java/com/sysco/WebAutomationAssignemnt/functions/Login.java
@@ -0,0 +1,35 @@
+package com.sysco.WebAutomationAssignemnt.functions;
+
+import com.sysco.WebAutomationAssignemnt.common.Constants;
+import com.sysco.WebAutomationAssignemnt.pages.LoginPage;
+import com.sysco.WebAutomationAssignemnt.utils.DriverSetUpUtil;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by Rifad on 5/21/18.
+ */
+public class Login  {
+
+    public static LoginPage ogmLoginPage = new LoginPage();
+
+
+    public static void loadLoginPage() {
+
+        if (Constants.RUN_LOCALLY) {
+            DriverSetUpUtil.setToRunLocally();
+            DesiredCapabilities capabilities = null;
+            ogmLoginPage.loadLoginPage(capabilities, Constants.APP_URL);
+        } else {
+            ogmLoginPage.loadLoginPage(DriverSetUpUtil.setToRunRemotely(Constants.APP_OS), Constants.APP_URL);
+        }
+    }
+
+
+    public static void quiteDriver() {
+        ogmLoginPage.quitDriver();
+    }
+
+    public static void loginToFtr() {
+
+    }
+}
---
+++ src/test/java/com/sysco/WebAutomationAssignemnt/tests/LoginTest.java
@@ -0,0 +1,31 @@
+package com.sysco.WebAutomationAssignemnt.tests;
+
+
+import com.sysco.WebAutomationAssignemnt.data.LoginData;
+import com.sysco.WebAutomationAssignemnt.functions.Login;
+import com.sysco.WebAutomationAssignemnt.utils.ExcelUtil;
+import com.sysco.WebAutomationAssignemnt.utils.TestBase;
+import org.testng.ITestContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+public class LoginTest extends TestBase {
+
+    @BeforeClass
+    public void init(ITestContext iTestContext) {
+        iTestContext.setAttribute("feature", "Login - ValidLogin");
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+
+        // Sample way to retrive data from excel
+        LoginData loginData = ExcelUtil.getLoginData("$as238l");
+
+        //UI Automation  sample
+        Login.loadLoginPage();
+        Login.quiteDriver();
+
+    }
+}
