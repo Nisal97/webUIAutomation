815d312d8544c0cfa1efe4813fce55df79b2bde1
Nisal Muthugala <nmmuthugala@gmail.com>
2018-08-08 09:11:33 +0530

    test bottleshop

--- src/main/java/com/sysco/WebAutomationAssignemnt/pages/AccountPage.java
+++ src/main/java/com/sysco/WebAutomationAssignemnt/pages/AccountPage.java
@@ -12,7 +12,7 @@ public class AccountPage extends BasePage {
     private By remove1 = By.id("cartheader-item-312683");
     private By remove2 = By.id("cartheader-item-312710");
     private By product = By.xpath("//*[@id=\"nav-wrapper\"]/div/ul[1]/li[1]/a");
-    private By bottleShop = By.xpath("/html/body/div[2]/div[3]/div[3]/div/div[2]/div[1]/div[1]/div[1]/div[1]/a/img");
+    private By bottleShop = By.xpath("//*[@id=\"nav-wrapper\"]/div/ul[1]/li[1]");
     private By exclusiveProducts = By.xpath("/html/body/div[2]/div[3]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a/img");
 
 //    WebElement loggedUser = syscoLabUIOgm.findElement(By.xpath("/html/body/div[2]/div[3]/div[2]/div[1]/div/div[3]/div[1]/h2"));
